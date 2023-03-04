17026296a0c3a994688cbc6dd75f37fddb8b971c
amrsamii <amrsamy411@gmail.com>
2023-03-04 23:08:26 +0200

    modified

--- apiapp/models.py
+++ apiapp/models.py
@@ -7,4 +7,13 @@ class awsimage(models.Model):
 
 # class test(models.Model):
 #     title=models.CharField(max_length=50)
+class Diseases(models.Model):
+    name=models.CharField(max_length=30)
+    on = models.ForeignKey('Fertilizers',blank=True,null=True,on_delete=models.CASCADE)
+
+    
+
+class Fertilizers(models.Model):
+    name=models.CharField(max_length=64)
+    Diseases=models.ForeignKey(Diseases,null=True,on_delete=models.DO_NOTHING)
 
